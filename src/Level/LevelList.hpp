677d6f138dd5bc70dc825501b9c2b7b0be5d52de
Roch Dionnet <roch.dionnet@univ-lyon1.fr>
2015-04-19 20:58:33 +0200

    Redone the level system. Still a lot to think about.

--- src/Level/LevelList.hpp
+++ src/Level/LevelList.hpp
@@ -5,15 +5,20 @@
 #include <vector>
 #include <Box2D/Box2D.h>
 #include "../Entity/AbstractEntity.hpp"
+/*
+typedef std::map<AbstractEntity*, b2Transform> LevelEntities;
+typedef std::vector<Level> LevelVector;
+
+struct Level {
+
+	LevelEntities entities;
+	bool endLevel(b2Body* const playerBody);
+}
 
 class LevelList {
 
-	protected:
-	typedef std::map<AbstractEntity*, b2Transform> Level;
-	typedef std::vector<Level> LevelVector;
-
 	public:
-
+	
 };
-
+ TODO redo this*/
 #endif
